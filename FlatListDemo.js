b4ec7b3094757ae845df1118da0b8437e6baf092
panda919 <stefan.velchev@hotmail.com>
2017-11-16 16:50:33 +0800

    I fixed auto load issed.

--- FlatListDemo.js
+++ FlatListDemo.js
@@ -8,88 +8,87 @@ import _ from 'lodash';
 const uuidv4 = require('uuid/v4');
 const { height, width } = Dimensions.get('window');
 var deepcopy = require("deepcopy");
-
 // list of images
 const testdata = [
   {
     uri: 'https://s-media-cache-ak0.pinimg.com/736x/b1/21/df/b121df29b41b771d6610dba71834e512.jpg',
-    caption:"this is test",
+    caption:"01this is test",
   },
   {
     uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQpD8mz-2Wwix8hHbGgR-mCFQVFTF7TF7hU05BxwLVO1PS5j-rZA',
-    caption:"this is test",
+    caption:"02this is test",
   },
   {
     uri: 'https://s-media-cache-ak0.pinimg.com/736x/5a/15/0c/5a150cf9d5a825c8b5871eefbeda8d14.jpg',
-    caption:"this is test",
+    caption:"t03his is test",
   },
   {
     uri: 'https://s-media-cache-ak0.pinimg.com/736x/04/63/3f/04633fcc08f9d405064391bd80cb0828.jpg',
-    caption:"this is test",
+    caption:"04this is test",
   },
   {
     uri: 'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQRWkuUMpLyu3QnFu5Xsi_7SpbabzRtSis-_QhKas6Oyj3neJoeug',
-    caption:"this is test",
+    caption:"05this is test",
   },
   {
     uri: 'https://s-media-cache-ak0.pinimg.com/736x/a5/c9/43/a5c943e02b1c43b5cf7d5a4b1efdcabb.jpg',
-    caption:"this is test",
+    caption:"06this is test",
   },
   {
     uri: 'https://i0.wp.com/www.youbodyhealth.com/wp-content/uploads/2016/08/Delicious-Foods-can-Harm-Your-Brain.jpg?',
-    caption:"this is test",
+    caption:"07this is test",
   },
   {
     uri: 'https://img.buzzfeed.com/buzzfeed-static/static/2017-03/29/15/campaign_images/buzzfeed-prod-fastlane-03/26-delicious-korean-foods-you-need-in-your-life-2-30138-1490814365-13_dblbig.jpg',
-    caption:"this is test",
+    caption:"08this is test",
   },
   {
     uri: 'https://pbs.twimg.com/media/B59AOmICQAAiGGj.png',
-    caption:"this is test",
+    caption:"09this is test",
   },
   {
     uri: 'https://img.buzzfeed.com/buzzfeed-static/static/2013-12/enhanced/webdr05/17/17/enhanced-buzz-orig-2548-1387320822-8.jpg',
-    caption:"this is test",
+    caption:"10this is test",
   },
   {
     uri: 'https://img.buzzfeed.com/buzzfeed-static/static/2015-03/17/15/enhanced/webdr13/enhanced-6527-1426620797-18.jpg',
-    caption:"this is test",
+    caption:"11this is test",
   },
   {
     uri: 'https://img.buzzfeed.com/buzzfeed-static/static/2014-12/1/15/enhanced/webdr02/enhanced-18393-1417466529-5.jpg',
-    caption:"this is test",
+    caption:"12this is test",
   },
   {
     uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXXTmdaGSOFK8iBeYqoA6_XiQGGWvu6KGnqAxXYyvJA-JKin8ImQ',
-    caption:"this is test",
+    caption:"13this is test",
   },
   {
     uri: 'https://img.buzzfeed.com/buzzfeed-static/static/2015-04/3/15/enhanced/webdr06/enhanced-24427-1428089292-2.jpg',
-    caption:"this is test",
+    caption:"14this is test",
   },
   {
     uri: 'https://img.buzzfeed.com/buzzfeed-static/static/2016-12/28/12/asset/buzzfeed-prod-web-09/sub-buzz-24236-1482944714-1.jpg',
-    caption:"this is test",
+    caption:"15this is test",
   },
   {
     uri: 'https://img.buzzfeed.com/buzzfeed-static/static/2016-03/7/17/enhanced/webdr08/enhanced-buzz-8155-1457391039-5.jpg',
-    caption:"this is test",
+    caption:"16this is test",
   },
   {
     uri: 'https://img.buzzfeed.com/buzzfeed-static/static/2017-03/30/12/asset/buzzfeed-prod-fastlane-01/sub-buzz-24597-1490890739-1.jpg',
-    caption:"this is test",
+    caption:"17this is test",
   },
   {
     uri: 'https://img.buzzfeed.com/buzzfeed-static/static/2016-01/14/20/campaign_images/webdr15/which-delicious-mexican-food-item-are-you-based-o-2-20324-1452822970-1_dblbig.jpg',
-    caption:"this is test",
+    caption:"18this is test",
   },
   {
     uri: 'https://img.buzzfeed.com/buzzfeed-static/static/2015-11/30/10/enhanced/webdr15/enhanced-18265-1448896942-17.jpg',
-    caption:"this is test",
+    caption:"19this is test",
   },
   {
     uri: 'https://img.buzzfeed.com/buzzfeed-static/static/2015-12/30/16/enhanced/webdr04/enhanced-15965-1451509932-6.jpg',
-    caption:"this is test",
+    caption:"20this is test",
   }
 ];
 const addData = [
@@ -156,30 +155,25 @@ class FlatListDemo extends Component {
       refreshing: false
     };
     makeRemoteRequest = this.makeRemoteRequest.bind(this);
+    onEndReachedCalledDuringMomentum = true;
   }
   componentDidMount() {
     this.makeRemoteRequest();
   }
   makeRemoteRequest = () => {
-    this.setState({ loading: true });
-   
+    console.log('Remote API Called!')
+
+    this.setState({ loading: true });   
     var appendedData = deepcopy(testdata);
     _.map(appendedData, function(e, i) {
-      height = Math.round(Math.random() * 100 + 100);      
-      // Image.getSize(e.uri, (width, height) => {
-      //   console.log("width:  " + width + "/  height:  " + height);
-      // });
+      height = Math.round(Math.random() * 100 + 100);            
       return _.extend(e, {index: uuidv4(),height: height});
-      //generate unique key, it is necessary to test only.
     });
-    //setTimeout(() => {
-      this.setState({     
-        loading: false,
-        refreshing: false,
-        //data: appendedData
-       data: [...this.state.data, ...appendedData]        
-       });
-    //}, 2000);  
+    this.setState({     
+      loading: false,
+      refreshing: false,
+      data: [...this.state.data, ...appendedData]        
+    });
   };
   handleRefresh = () => {
     this.setState(
@@ -192,41 +186,24 @@ class FlatListDemo extends Component {
     );
   };
   handleLoadMore = () => {
-    this.setState(
-      {       
-      },
-      () => {
-        this.makeRemoteRequest();
-      }
-    );
+    if (!this.onEndReachedCalledDuringMomentum) {
+      this.makeRemoteRequest();
+      this.onEndReachedCalledDuringMomentum = true;
+    }  
   };
   renderSeparator = () => {
     return (
-      <View
-        style={{
-          height: 10,          
-          backgroundColor: "#FFFFFF",
-         
-        }}
-      />
+      <View style={styles.headercontent} />
     );
   };
   renderFooter = () => {
     if (!this.state.loading) return null;
-
     return (
-      <View
-        style={{
-          paddingVertical: 20,
-          borderTopWidth: 1,
-          borderColor: "#CED0CE"
-        }}
-      >
-        <ActivityIndicator animating size="large" />
+      <View style={styles.footcontent}>
+          <ActivityIndicator animating size="large" />
       </View>
     );
   };
-  //          onEndReached={this.handleLoadMore}:)
   render() {
     return (
       <View style={{flexGrow:1, backgroundColor:'white'}}>
@@ -240,13 +217,13 @@ class FlatListDemo extends Component {
             ItemSeparatorComponent={this.renderSeparator}       
             onRefresh={this.handleRefresh}
             refreshing={this.state.refreshing}
-            onEndReachedThreshold={0}
+            onEndReachedThreshold={0.5}
             getHeightForItem={({ item }) => item.height + 2}
             onEndReached={this.handleLoadMore}
             numColumns={3}
+            onMomentumScrollBegin={() => { this.onEndReachedCalledDuringMomentum = false; }}            
           />
-        </List>
-      
+        </List>      
       </View>
     );
   }
@@ -257,6 +234,15 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
+  },
+  footcontent: {
+    paddingVertical: 20,
+    borderTopWidth: 1,
+    borderColor: "#CED0CE"
+  },
+  headercontent: {
+    height: 10,          
+    backgroundColor: "#FFFFFF", 
   },
   gradient: {
     width: 200,
